b45ad682b8de65df50d194d5156714cc4ee3d291
Jeet3714 <55792117+Jeet3714@users.noreply.github.com>
2020-08-10 20:58:52 +0530

    Add files via upload

---
+++ CourseraCapstone_Week1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>This Notebook is Exclusively For IBM Data Science Professional Certificate Capstone Project</h1><br> <center><h2>This is Week 1 of Coursera Capstone Project</h2></center>
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
